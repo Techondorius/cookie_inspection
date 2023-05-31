8af411fdd89b59d20e2b7daaface4f603a6453f0
Kyosuke Fujita <kurofuku01@gmail.com>
2023-05-31 21:30:52 +0900

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	g := gin.Default()
+
+	g.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3001"},
+		AllowCredentials: true,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	}))
+
+	g.GET("/setcookie", func(c *gin.Context) {
+		c.SetSameSite(http.SameSiteNoneMode)
+		c.SetCookie("gin_cookie", "test", 3600, "/", "192.168.10.103", true, true)
+		c.JSON(200, gin.H{"msg": "OK"})
+	})
+
+	g.GET("/getcookie", func(c *gin.Context) {
+		cookie, err := c.Cookie("gin_cookie")
+		if err != nil {
+			fmt.Println("cookie not found")
+			return
+		}
+		fmt.Println("cookie value: ", cookie)
+		c.JSON(200, gin.H{"msg": "OK"})
+	})
+
+	g.Run(":8080")
+}
